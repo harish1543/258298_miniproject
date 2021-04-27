bcf43ceb21f8a21e02aad0510808a936d921596b
Harish <harish24263@gmail.com>
2021-04-27 21:52:26 +0530

    Updated Codefiles

---
+++ src/BubbleSort.py
@@ -0,0 +1,8 @@
+def BubbleSort(Input_list,List_size):
+    for i in range(List_size-1):
+        for j in range(List_size-i-1):
+            if Input_list[j]>Input_list[j+1]:
+                Input_list[j], Input_list[j+1] = Input_list[j+1], Input_list[j]
+    return Input_list
+if __name__ == "main":
+    print("Bubble sort")
---
+++ src/InsertionSort.py
@@ -0,0 +1,12 @@
+def InsertionSort(Input_list,List_size):
+    for i in range(List_size):
+        key = Input_list[i]
+        j = i-1
+        while(j >= 0 and Input_list[j]>key):
+            Input_list[j+1] = Input_list[j]
+            j-=1
+        Input_list[j+1] = key
+    return Input_list
+
+if __name__ =="main":
+    print("Insertion Sort")
